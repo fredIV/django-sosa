2348a1bc8f286a70dbf47228a1927b9db24e3892
Fred <fredcmdy@gmail.com>
2018-11-15 00:35:31 -0500

    update database, piplock file, and home

--- sosa_project/settings.py
+++ sosa_project/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'sosa_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoSosa',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'PORT': '5432'
     }
 }
 
