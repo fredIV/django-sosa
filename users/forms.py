eb350ba4aa1936d102d3c0465fa4e05545b28f6f
Joel Witherspoon <jw09227@georgiasouthern.edu>
2018-11-25 20:05:06 -0500

    Added extra stimulus functionality from old project

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from pages import models
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
@@ -15,3 +16,11 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'username',)
+
+
+# class PostBoard(forms.ModelForm):
+#
+#     class Meta:
+#         model = models.board
+#         fields = ('boardR', 'boardB', 'boardG', 'backgroundR', 'backgroundG', 'backgroundB', 'board_tilt_x',
+#           'board_tilt_y', 'admin_ID')
